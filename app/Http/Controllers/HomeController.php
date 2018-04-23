459e9066c407379fc40868121b55ac19f555f5db
unknown <level@levelcoding.com>
2018-04-23 13:46:57 +0300

    “test”

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Area;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $areas = Area::get()->toTree();
+
+        //return $areas;
+        return view('home', compact('areas'));
+    }
+}
